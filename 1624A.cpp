39422e4d6ede56edab7e4466981f79bdaeca61ee
evankendrick <kendrickevan131@gmail.com>
2026-01-12 19:10:13 +0700

    Add files via upload

---
+++ 1624A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, mx, mn, x;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		mn = INT_MAX, mx = INT_MIN;
+
+		for (int j = 0; j < n; j++) {
+			cin >> x;
+			mn = min(mn, x);
+			mx = max(mx, x);
+		}
+		cout << mx - mn << endl;
+	}
+}
